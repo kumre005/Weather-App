b290d5cd2d1ca3734b11f7dafbda3dc261aee7de
kumre005 <kumre005@gmail.com>
2025-03-23 21:58:21 +0530

    index add

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '1f6e70ba57mshaa82b678fc92b04p1a6a23jsn53dba45a5226',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
