d3e836e065f7133f641af6eb98550eafa131daab
kumre005 <kumre005@gmail.com>
2025-04-21 21:18:42 +0530

    Add lines

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const url = 'https://yahoo-weather5.p.rapidapi.com/weather?location=Warora&format=json&u=f';
 const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '1f6e70ba57mshaa82b678fc92b04p1a6a23jsn53dba45a5226',
-		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+		'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+async function getWeather() {
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
 }
+getWeather();
